1e3c8750ea0d1979c2410b9c8d54f2813331b054
Rizkifirmansyah19 <firmansyahrizki2020@gmail.com>
2021-05-06 00:05:06 +0700

    TUGAS PBO

---
+++ Rizki firmansyah DESIMAL KE HEXADESIMALl/desimal ke hexadesimal rizki firmansyah/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercise30
+{
+    public static void Main()
+    {
+
+
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Tugas Program Bilangan Desimal Ke Hexadesimal Visual C#");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Nama      : Rizki Firmansyah");
+        Console.WriteLine("Npm       : 1101201062");
+        Console.WriteLine("Kelas     : Malam");
+        Console.WriteLine("Dosen     : Asep Mabrur Aid,S.T,M.Kom");
+        Console.WriteLine("Prodi     : Teknik Informatika");
+        Console.WriteLine("MK        : Pemgraman Berorientasi Objek");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        string hexval = "4B0";
+        Console.WriteLine("Hexadecimal number: " + hexval);
+        int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+        Console.WriteLine("Convert to-");
+        Console.WriteLine("Decimal number: " + decValue);
+    }
+}
+
